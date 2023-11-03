0145d4fada64d0b0b9cde61bf21a417dc2c2798a
leandroltfx <leandrobsb55@hotmail.com>
2023-11-03 08:20:44 -0300

    iniciando feature login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'social-network-front-angular';
-}
+export class AppComponent { }
