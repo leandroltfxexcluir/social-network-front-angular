9e98199b0f1ee68fe3d5ddecab4c657a72510904
leandroltfx <leandrobsb55@hotmail.com>
2023-11-04 18:09:18 -0300

    removendo módulo não utilizado

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -17,7 +17,6 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
 
     MatCardModule,
