f23fefb6d49ac1fa52d88ff8f86fb6608fbca1e4
leandroltfx <leandrobsb55@hotmail.com>
2023-11-04 17:55:49 -0300

    criando layout do login

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
 
 @NgModule({
   declarations: [
@@ -11,6 +17,15 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+
     LoginRoutingModule
   ]
 })
