7ba0aa4d9d4427c200633c8d6debc121c606cbbb
leandroltfx <leandrobsb55@hotmail.com>
2023-11-05 17:45:20 -0300

    iniciando feature de esqueci a senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'cadastro', loadChildren: () => import('./features/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
+  { path: 'esqueci-a-senha', loadChildren: () => import('./features/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
   { path: '**', redirectTo: 'login' }
 ];
 
