1f80dc8080b5eeb9c21f80c17f04fab935aae5be
leandroltfx <leandrobsb55@hotmail.com>
2023-11-05 09:28:56 -0300

    iniciando feature de cadastro de usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
+  { path: 'cadastro', loadChildren: () => import('./features/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
   { path: '**', redirectTo: 'login' }
 ];
 
