73d9f2484de71fb39cb3ae31d7dbc5741fe4bff9
leandroltfx <leandrobsb55@hotmail.com>
2023-11-05 17:37:05 -0300

    criando formulário de cadastro de usuário

--- src/app/features/user-registration/user-registration.module.ts
+++ src/app/features/user-registration/user-registration.module.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { UserRegistrationRoutingModule } from './user-registration-routing.module';
 import { UserRegistrationComponent } from './user-registration.component';
@@ -10,6 +17,13 @@ import { UserRegistrationComponent } from './user-registration.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
 
     UserRegistrationRoutingModule
   ]
