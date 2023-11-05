63f0a638f34ae35484a80be2325add68b5a67bb2
leandroltfx <leandrobsb55@hotmail.com>
2023-11-05 17:55:00 -0300

    criando formulário do esqueci a senha

--- src/app/features/forgot-password/forgot-password.component.ts
+++ src/app/features/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sn-forgot-password',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  forgotPasswordForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.forgotPasswordForm = this.buildUserRegistrationForm();
+  }
+
+  receiveCode(): void {
+    if (this.forgotPasswordForm.valid) {
+      console.log('form valid');
+    } else {
+      console.log('form invalid');
+    }
+  }
+
+  buildUserRegistrationForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        email: [null, [Validators.required]],
+      }
+    );
+  }
+
+  getErrorMessage(control: any, message: string) {
+    if (control.hasError('required')) {
+      return message;
+    }
+
+    return '';
   }
 
 }
