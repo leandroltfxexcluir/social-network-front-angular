2cd02f77bd1fc2c9688336cf968ea24aa55fd7d4
leandroltfx <leandrobsb55@hotmail.com>
2023-11-06 15:42:56 -0300

    importando módulos necessários no teste do esqueci a senha

--- src/app/features/forgot-password/forgot-password.component.spec.ts
+++ src/app/features/forgot-password/forgot-password.component.spec.ts
@@ -1,4 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
@@ -8,6 +16,16 @@ describe('ForgotPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+      ],
       declarations: [ ForgotPasswordComponent ]
     })
     .compileComponents();
