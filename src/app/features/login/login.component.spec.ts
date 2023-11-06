1a24a052ca6561d655096790b03b5440a5d96bc2
leandroltfx <leandrobsb55@hotmail.com>
2023-11-06 15:43:27 -0300

    importando módulo de roteamento no teste do login

--- src/app/features/login/login.component.spec.ts
+++ src/app/features/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -19,6 +20,7 @@ describe('LoginComponent', () => {
       imports: [
         BrowserAnimationsModule,
         ReactiveFormsModule,
+        RouterTestingModule,
     
         MatCardModule,
         MatFormFieldModule,
